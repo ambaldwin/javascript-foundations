fca1cf5983186479bf517e68bb854dc0f1f2c17c
Anna Baldwin <abaldwin@developers.wyyerd.com>
2022-12-02 15:18:34 -0700

    complete dragon and vampire classes

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+    constructor(name, rider, hungry = true) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = hungry;
+        this.eatCount = 0;
+    }
+
+    greet() {
+        return `Hi, ${this.rider}!`
+    }
+
+    eat() {
+        this.eatCount++;
+        if (this.eatCount >= 3) this.hungry = false;
+    }
+}
+
+module.exports = Dragon;
+
 

8068713fc94cd62605ab4c05e00707917a6e6d8d
Anna Baldwin <abaldwin@developers.wyyerd.com>
2022-12-02 17:19:50 -0700

    add medusa, person, statue classes

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,21 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(name) {
+        this.name = name;
+        this.statues = [];
+    }
+
+    gazeAtVictim(victim) {
+        let statue = new Statue(victim.name)
+        this.statues.push(statue);
+
+        if (this.statues.length === 4) {
+            let freed = this.statues.shift();
+            return new Person(freed.name, 'relieved');
+        }
+    }
+}
+
+module.exports = Medusa;
